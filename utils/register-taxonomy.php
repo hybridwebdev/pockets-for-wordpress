360ec75ab33c7353493332a3fade91545304bdc3
hybridwebdev <justin_a_lindsay@hotmail.com>
2026-06-17 05:46:41 +0000

    removed translation fn from register cpt/taxonomy

--- utils/register-taxonomy.php
+++ utils/register-taxonomy.php
@@ -1,45 +1,60 @@
 <?php
+
 namespace pockets\utils;
+
 class register_taxonomy {
 
-    function __construct ( 
-        $taxonomy, 
-        $post_type, 
-        $tax_slug = null , 
-        $tax_title=null, 
-        $args = []
+    function __construct(
+        string $taxonomy,
+        string $post_type,
+        string | null $tax_slug = null,
+        string | null $tax_title = null,
+        array $args = []
     ) {
-        
-        $tax_title =  ($tax_title) ? $tax_title : ucwords((str_replace("_", " ", $taxonomy)));
-        $tax_slug =  ($tax_slug) ? $tax_slug : $taxonomy;
-        $stripped_tax = ucwords((str_replace("_", " ", $taxonomy))); 
 
-        $merged = wp_parse_args( $args, [
+        $tax_title = $tax_title
+            ? $tax_title
+            : ucwords(str_replace('_', ' ', $taxonomy));
+
+        $tax_slug = $tax_slug
+            ? $tax_slug
+            : $taxonomy;
+
+        $stripped_tax = ucwords(str_replace('_', ' ', $taxonomy));
+
+        $merged = wp_parse_args($args, [
             'hierarchical' => true,
+
             'labels' => [
-                'name' => _x( $tax_title, 'taxonomy general name' ),
-                'singular_name' => _x( 'Category', 'taxonomy singular name' ),
-                'search_items'      =>__("Search {$stripped_tax} Types"),
-                'popular_items'     =>__("Popular {$stripped_tax} Types"),
-                'all_items'         =>__("All {$stripped_tax} Types"),
-                'parent_item'       =>__( "Parent {$stripped_tax} Types" ),
-                'parent_item_colon' =>__( "Parent {$stripped_tax} Type:" ),
-                'edit_item'         =>__( "Edit {$stripped_tax} Type" ),
-                'update_item'       =>__( "Update {$stripped_tax} Type" ),
-                'add_new_item'      =>__( "Add New {$stripped_tax} Type" ),
-                'new_item_name'     =>__( "New {$stripped_tax} Type Name" )
-                        
+                'name'               => sprintf('%s', $tax_title),
+                'singular_name'      => sprintf('%s', $tax_title),
+                'search_items'       => sprintf('Search %s Types', $stripped_tax),
+                'popular_items'      => sprintf('Popular %s Types', $stripped_tax),
+                'all_items'          => sprintf('All %s Types', $stripped_tax),
+                'parent_item'        => sprintf('Parent %s Types', $stripped_tax),
+                'parent_item_colon'  => sprintf('Parent %s Type:', $stripped_tax),
+                'edit_item'          => sprintf('Edit %s Type', $stripped_tax),
+                'update_item'        => sprintf('Update %s Type', $stripped_tax),
+                'add_new_item'       => sprintf('Add New %s Type', $stripped_tax),
+                'new_item_name'      => sprintf('New %s Type Name', $stripped_tax),
             ],
-            'show_ui'=>true,
-            'rewrite' => [ 'slug' => $tax_slug  ],
-            'public'                     => true,
-            'show_admin_column'          => true,
-            'show_in_nav_menus'          => true,
-            'show_tagcloud'              => true
+
+            'show_ui'              => true,
+            'rewrite'              => [
+                'slug' => $tax_slug,
+            ],
+            'public'               => true,
+            'show_admin_column'    => true,
+            'show_in_nav_menus'    => true,
+            'show_tagcloud'        => true,
         ]);
 
-        register_taxonomy($taxonomy, $post_type, $merged);
-                    
-    }             
+        register_taxonomy(
+            $taxonomy,
+            $post_type,
+            $merged
+        );
+
+    }
 
 }
